1bec5fb60e92ba7b44ba2897e517d448fc3097e3
James Furey <jfurey@gmail.com>
2022-11-18 14:18:46 +1100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,110 @@
+# secret-santa-recipient-generator
+
+Secret Santa participant ↔︎ recipient generator.
+
+## Contents
+
+- [Quick Start](#quick-start)
+- [Configuration](#configuration)
+- [Node Scripts](#node-scripts)
+
+## Quick Start
+
+- [Configure](#environment-variables) `.env` file
+- Ensure [Node](https://nodejs.org/en/download) running:<br>`$ node -v` >= `16.16.*`
+- Ensure [NPM](https://docs.npmjs.com/downloading-and-installing-node-js-and-npm) running:<br>`$ npm -v` >= `8.14.*`
+- Install dependencies:<br>
+  `$ npm ci`
+- Run generator:<br>
+  `$ npm run generate`
+
+## Configuration
+
+- [Participants JSON](#participants-json)
+- [Environment Variables](#environment-variables)
+
+### Participants JSON
+
+Create a `participants.json` file containing participant names:
+
+```json
+{
+  "james": null,
+  "angie": null,
+  "mum": null,
+  "carla": null,
+  "julia": null,
+  "drew": null,
+  "elise": null
+}
+```
+
+Optionally prefill recipient values where required:
+
+```json
+{
+  "james": {
+    "givingTo": "angie"
+  },
+  "angie": {
+    "givingTo": "elise"
+  },
+  "mum": null,
+  "carla": null,
+  "julia": null,
+  "drew": null,
+  "elise": null
+}
+```
+
+**Note:** The suggested `participants.json` file location is:
+
+```text
+./input/participants.json
+```
+
+### Environment Variables
+
+Create a root `.env` file with appropriate values:
+
+```ini
+PARTICIPANTS_PATH=
+LOG_LEVEL=
+```
+
+Example:
+
+```ini
+PARTICIPANTS_PATH=path/to/participants.json
+LOG_LEVEL=debug
+```
+
+**Note:** `LOG_LEVEL=debug` for verbose console output.
+
+## Node Scripts
+
+- [Generate](#generate)
+
+### Generate
+
+To run the generator:
+
+```console
+$ npm run generate
+```
+
+Example output:
+
+```text
+┌─────────┬──────────┬─────────────┐
+│ (index) │ givingTo │ gettingFrom │
+├─────────┼──────────┼─────────────┤
+│  james  │ 'angie'  │   'carla'   │
+│  angie  │ 'elise'  │   'james'   │
+│   mum   │  'drew'  │   'drew'    │
+│  carla  │ 'james'  │   'julia'   │
+│  julia  │ 'carla'  │   'elise'   │
+│  drew   │  'mum'   │    'mum'    │
+│  elise  │ 'julia'  │   'angie'   │
+└─────────┴──────────┴─────────────┘
+```
---
+++ index.js
@@ -0,0 +1,65 @@
+require('dotenv').config()
+
+const _ = require('lodash')
+const fs = require('fs-extra')
+
+const run = async () => {
+  let restart = true
+  while (restart) {
+    restart = false
+    const participants = getParticipants()
+    prefillGettingFrom(participants)
+    let i = 0
+    while (withoutGettingFrom(participants).length && !restart) {
+      try {
+        const [getter] = _.sample(withoutGettingFrom(participants))
+        const [giver] = _.sample(withoutGivingTo(participants, getter))
+        participants[getter].gettingFrom = giver
+        participants[giver].givingTo = getter
+        i++
+        console.debug(`Iteration ${i}:`)
+        console.debug(participants)
+      } catch (e) {
+        console.debug(`An error occured while assigning recipients ("${e.message}").`)
+        console.debug('Restarting…')
+        restart = true
+      }
+    }
+    console.table(participants)
+  }
+}
+
+console.debug = (...arguments) => {
+  if (process.env.LOG_LEVEL === 'debug') console.log(...arguments)
+}
+
+const getParticipants = () => {
+  const { PARTICIPANTS_PATH } = process.env
+  if (!fs.pathExistsSync(PARTICIPANTS_PATH)) throw new Error ('PARTICIPANTS_PATH does not exist.')
+  const source = fs.readJsonSync(PARTICIPANTS_PATH)
+  if (!source || !Object.entries(source).length) throw new Error('PARTICIPANTS_PATH content invalid.')
+  return _.mapValues(source, options => ({ givingTo: options?.givingTo, gettingFrom: undefined }))
+}
+
+const prefillGettingFrom = participants => {
+  Object.entries(participants).forEach(participantEntry => {
+    const [ name, actions ] = participantEntry
+    if (actions.givingTo) participants[actions.givingTo].gettingFrom = name
+  })
+}
+
+const withoutGettingFrom = participants =>
+  Object.entries(participants)
+    .filter(participantEntry => !participantEntry[1].gettingFrom)
+
+const withoutGivingTo = (participants, excludingParticipant = undefined) =>
+  Object.entries(participants)
+    .filter(participantEntry => !participantEntry[1].givingTo && participantEntry[0] !== excludingParticipant)
+
+const onError = e => {
+  console.log(e)
+  console.log('Exiting…')
+  process.exit(1)
+}
+
+run().catch(onError)
